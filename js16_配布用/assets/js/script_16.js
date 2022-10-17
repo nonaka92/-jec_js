12123bdcba70d9da125ef964faed30a1e912bd02
nonaka kohei <22aw0129@jec.ac.jp>
2022-10-17 10:03:45 +0900

    firast commit

---
+++ js16_配布用/assets/js/script_16.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function fvTextInsert() {
+  const fvCountText = '第' + fvCountNum + '回まで実施';
+  const fvCountNum = 1;
+  const fvText = 'このサイトはJavaScriptの授業で構築しながら<br>実際の実装について学んでいくためのサイトです。';
+}
